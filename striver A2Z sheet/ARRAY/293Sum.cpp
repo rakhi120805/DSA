f8728456d2600ea3c0e5ab800ba080e934ff01d6
rakhi120508 <rakhikumarii7277@gmail.com>
2026-03-25 23:25:52 +0530

    3 Sum optimal approach

--- striver A2Z sheet/ARRAY/293Sum.cpp
+++ striver A2Z sheet/ARRAY/293Sum.cpp
@@ -25,22 +25,54 @@ using namespace std;
 
 // Better Approach
 
+// vector<vector<int>> threeSum(vector<int>&arr){
+//     int n=arr.size();
+//     set<vector<int>>s;
+//     for(int i=0;i<n;i++){
+//         set<int>mpp;
+//         for(int j=i + 1;j<n;j++){
+//             int third=-(arr[i] + arr[j]);
+//             if(mpp.find(third)!=mpp.end()){
+//                 vector<int>temp;
+//                 temp.push_back(arr[i]);
+//                 temp.push_back(arr[j]);
+//                 temp.push_back(third);
+//                 sort(temp.begin(),temp.end());
+//                 s.insert(temp);
+//             }
+//             mpp.insert(arr[j]);
+//         }
+//     }
+//     vector<vector<int>>ans(s.begin(),s.end());
+//     return ans;
+// }
+
+// OPTIMAL APPROACH
 vector<vector<int>> threeSum(vector<int>&arr){
+    sort(arr.begin(),arr.end());
     int n=arr.size();
     set<vector<int>>s;
     for(int i=0;i<n;i++){
-        set<int>mpp;
-        for(int j=i + 1;j<n;j++){
-            int third=-(arr[i] + arr[j]);
-            if(mpp.find(third)!=mpp.end()){
-                vector<int>temp;
-                temp.push_back(arr[i]);
-                temp.push_back(arr[j]);
-                temp.push_back(third);
-                sort(temp.begin(),temp.end());
-                s.insert(temp);
+        if(i>0 && arr[i]==arr[i-1])continue;
+        int j=i+1;
+        int k=n-1;
+        
+        while(j<k){
+            int sum=arr[i]+arr[j]+arr[k];
+            if(sum==0){
+                vector<int>v={arr[i],arr[j],arr[k]};
+                s.insert(v);
+                j++;
+                k--;
+                while(j<k && arr[j]==arr[j-1])j++;
+                while(j<k && arr[k]==arr[k+1])k--;
             }
-            mpp.insert(arr[j]);
+            else if(sum<0){
+                j++;
+            }
+            else{
+                k--;
+            }
         }
     }
     vector<vector<int>>ans(s.begin(),s.end());
