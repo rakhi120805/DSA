c7b6344f0073d09facff7db8cff3537ba8cdc912
rakhi120508 <rakhikumarii7277@gmail.com>
2026-04-04 19:58:07 +0530

    Insert at a position

---
+++ striver A2Z sheet/BINARY_SEARCH/4InsertAtIndex.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int insertAtIndex(vector<int>&arr,int target){
+    int n=arr.size();
+    int low=0;
+    int high=n-1;
+    int ans=n;
+    while(low<=high){
+        int mid=low + (high - low)/2;
+        if(arr[mid]==target){
+            return mid;
+        }
+        else if(arr[mid]>target){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+    int ans=insertAtIndex(arr,target);
+    cout<<ans;
+    return 0;
+}
