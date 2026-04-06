dd93dec87c46701525dc847bdfce5eea72f4e5f7
rakhi120508 <rakhikumarii7277@gmail.com>
2026-04-07 00:33:51 +0530

    count the number of occurrences

---
+++ striver A2Z sheet/BINARY_SEARCH/7.countNumberOfOccurrences.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int firstIndex(vector<int>&nums,int target){
+    int n=nums.size();
+    int low=0;
+    int high=n-1;
+    int fi=-1;
+    while(low<=high){
+        int mid=low+(high - low)/2;
+        if(nums[mid]==target){
+            fi=mid;
+            high=mid-1;
+        }
+        else if(nums[mid]>target){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return fi;
+}
+
+int lastIndex(vector<int>&nums,int target){
+    int n=nums.size();
+    int low=0;
+    int high=n-1;
+    int li=-1;
+    while(low<=high){
+        int mid=low+(high - low)/2;
+        if(nums[mid]==target){
+            li=mid;
+            low=mid+1;
+        }
+        else if(nums[mid]>target){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return li;
+}
+
+int count(vector<int>&arr,int target){
+    int fi=firstIndex(arr,target);
+    int li=lastIndex(arr,target);
+    if(fi==-1 && li==-1)return 0;
+    return (li - fi)+1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+    int ans=count(arr,target);
+    cout<<ans;
+}
