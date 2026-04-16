a6244c80e9f6561c66cad165f450726eaab800ba
rakhi120508 <rakhikumarii7277@gmail.com>
2026-04-16 20:41:24 +0530

    Search in rotated sorted array

---
+++ questions/leetcodeque/33.search-in-rotated-sorted-array.cpp
@@ -0,0 +1,46 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int search(vector<int>& nums, int target) {
+    int n=nums.size();
+    int low=0;
+    int high=n-1;
+    while(low<=high){
+        int mid=low+(high - low)/2;
+        if(nums[mid]==target)return mid;
+        else if(nums[low]<=nums[mid]){
+            if(nums[low]<=target && nums[mid]>target){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+        else{
+            if(nums[high]<=target && nums[mid]>target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int target;
+    cin>>target;
+    int ans=search(arr,target);
+    cout<<ans;
+}
+
+
+
