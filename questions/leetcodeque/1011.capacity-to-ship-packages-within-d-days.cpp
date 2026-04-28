af6f0e699ca58455973772cdc81f90437859f45c
rakhi120508 <rakhikumarii7277@gmail.com>
2026-04-29 01:21:44 +0530

    leetcode questions

---
+++ questions/leetcodeque/1011.capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1011 lang=cpp
+ *
+ * [1011] Capacity To Ship Packages Within D Days
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        
+    }
+};
+// @lc code=end
+
