b78232bacdb50e8b9bd58dea0282c85a7011aa03
rakhi120508 <rakhikumarii7277@gmail.com>
2026-05-03 02:05:28 +0530

    subarray with xor k

---
+++ striver A2Z sheet/ARRAY/31subarraywithxork.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int countSubarray(vector<int>&arr,int k){
+    
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    int ans = 0;
+    
+    cout<<ans;
+}
