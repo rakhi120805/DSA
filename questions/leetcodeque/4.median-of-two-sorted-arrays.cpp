8465d34e7c0ea82d606f35bd0a680b6b67ea7e79
rakhi120508 <rakhikumarii7277@gmail.com>
2026-05-25 01:09:52 +0530

    k th element in sorted arrays

--- questions/leetcodeque/4.median-of-two-sorted-arrays.cpp
+++ questions/leetcodeque/4.median-of-two-sorted-arrays.cpp
@@ -6,27 +6,35 @@ using namespace std;
         double ans=-1;
         int n=nums1.size();
         int m=nums2.size();
+        if(n>m)return findMedianSortedArrays(nums2,nums1);
         int low=0;
         int t=n+m;
-        int high=(t/2)-1;
+        // int fixed=t/2;
+        int high=n;
        
         while(low<=high){
             int mid=low+(high - low)/2;
-            int l1=mid - 1;
-            int el=(t/2)-(mid);
-            int l2=el-1;
-            int r1=mid;
-            int r2=el;
-            if(nums1[l1]<=nums2[r2] && nums2[l2]<=nums1[r1]){
-                int n1=max(nums1[l1],nums2[l2]);
-                int n2=min(nums1[r1],nums2[r2]);
-                return ans=(n1+n2)/2;
+            int el=((n + m + 1)/2) - mid;
+            int l1=((mid)==0?INT_MIN:nums1[mid-1]);
+            
+            int l2=((el)==0?INT_MIN:nums2[el-1]);
+            int r1=((mid)==n?INT_MAX:nums1[mid]);
+            int r2=((el)==m?INT_MAX:nums2[el]);
+            if(l1<=r2 && l2<=r1){
+                if(t%2==0){
+                int n1=max(l1,l2);
+                int n2=min(r1,r2);
+                return (n1+n2)/2.0;
+                }
+                else{
+                    return max(l1,l2);
+                }
                 
             }
-            else if(nums1[l1]>nums2[r2])high=mid-1;
+            else if(l1>r2)high=mid-1;
             else low=mid+1;
         }
-        return ans;
+        return 0.0;
     }
 int main(){
     int n;
@@ -44,5 +52,5 @@ int main(){
     
     double ans = findMedianSortedArrays(arr1,arr2);
     
-    cout<<ans;
+    cout  << ans;
 }
