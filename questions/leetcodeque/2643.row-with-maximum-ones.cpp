b5dda8c0682b79a038cfbbf4002fe022fcd8e121
rakhi120508 <rakhikumarii7277@gmail.com>
2026-05-28 00:27:11 +0530

    optimized

---
+++ questions/leetcodeque/2643.row-with-maximum-ones.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> rowAndMaximumOnes(vector<vector<int>>& mat) {
+        
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int>arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+    int ans = 0;
+    
+    cout<<ans;
+}
+
--- questions/leetcodeque/74.search-a-2-d-matrix.cpp
+++ questions/leetcodeque/74.search-a-2-d-matrix.cpp
@@ -1,6 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
 
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    int n=matrix.size();
+    int m=matrix[0].size();
+    int low=0;
+    int high=(n*m)-1;
+    while(low<=high){
+        int mid=low+(high - low)/2;
+        int i=mid/m;
+        int j=mid%m;
+        if(matrix[i][j]==target){
+            return true;
+        }
+        else if(matrix[i][j]>target){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return false;
+   
+}
+int main(){
+    int n;
+    cin>>n;
+    int m;
+    cin>>m;
+    vector<vector<int>>arr(n,vector<int>(m));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin>>arr[i][j];
+        }
         
     }
+    int t;
+    cin>>t;
+    int ans = searchMatrix(arr,t);
+    
+    cout<<ans;
+}
+
 
 
