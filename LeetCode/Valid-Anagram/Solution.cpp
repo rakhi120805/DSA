e4d7ea234b8484610e1b78a874d2d7852fcbe8f9
Rakhi Kumari <151828091+rakhi120805@users.noreply.github.com>
2026-07-07 23:36:00 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.cpp
+++ LeetCode/Valid-Anagram/Solution.cpp
@@ -1,19 +1,23 @@
 1class Solution {
 2public:
 3    bool isAnagram(string s, string t) {
-4        vector<int>hash(26);
-5        for(int i=0;i<s.length();i++){
-6            int ch=s[i] - 'a';
-7            hash[ch]++;
-8        }
-9        for(int i=0;i<t.length();i++){
-10            int ch=t[i]-'a';
-11            hash[ch]--;
-12        }
-13        for(auto it:hash){
-14            if(it!=0)return false;
-15        }
-16return true;
-17        
-18    }
-19};
+4        sort(s.begin(),s.end());
+5        sort(t.begin(),t.end());
+6        if(s==t)return true;
+7        return false;
+8//         vector<int>hash(26);
+9//         for(int i=0;i<s.length();i++){
+10//             int ch=s[i] - 'a';
+11//             hash[ch]++;
+12//         }
+13//         for(int i=0;i<t.length();i++){
+14//             int ch=t[i]-'a';
+15//             hash[ch]--;
+16//         }
+17//         for(auto it:hash){
+18//             if(it!=0)return false;
+19//         }
+20// return true;
+21        
+22    }
+23};
