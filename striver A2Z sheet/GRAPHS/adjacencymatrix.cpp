3c5decd9181e0d125b08d864980f5ace1a2fae4f
rakhi120508 <rakhikumarii7277@gmail.com>
2026-07-23 01:56:10 +0530

    Adjacency matrix

---
+++ striver A2Z sheet/GRAPHS/adjacencymatrix.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin>>n>>m;
+    vector<vector<int>>adj(n+1,vector<int>(n+1));
+    for(int i=0;i<m;i++){
+        int u,v;
+        cin>>u>>v;
+        adj[u][v]=1;
+        adj[v][u]=1;
+    }
+
+    for(int i=0;i<n+1;i++){
+        for(int j=0;j<n+1;j++){
+            cout<<adj[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
