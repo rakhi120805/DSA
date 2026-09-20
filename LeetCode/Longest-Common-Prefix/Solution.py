40e1b7ce704694bf3cf4d6bf811eb23c3eee264c
Rakhi Kumari <151828091+rakhi120805@users.noreply.github.com>
2026-09-20 23:34:30 +0530

    Add solution for Longest Common Prefix

--- LeetCode/Longest-Common-Prefix/Solution.py
+++ LeetCode/Longest-Common-Prefix/Solution.py
@@ -4,13 +4,23 @@
 4        :type strs: List[str]
 5        :rtype: str
 6        """
-7        res=""
-8        for i in range(0,len(strs[0])):
-9            for j in range(1,len(strs)):
-10                if i>=len(strs[j]) or strs[j][i]!=strs[0][i]:
-11                    return res
+7        # res=""
+8        # for i in range(0,len(strs[0])):
+9        #     for j in range(1,len(strs)):
+10        #         if i>=len(strs[j]) or strs[j][i]!=strs[0][i]:
+11        #             return res
 12            
-13            res+=strs[0][i]
+13        #     res+=strs[0][i]
 14        
-15        return res
+15        # return res
 16        
+17        res=""
+18        for i in range(0,len(strs[0])):
+19            for j in range(1,len(strs)):
+20
+21                if i>=len(strs[j]) or strs[j][i]!=strs[0][i]:
+22                    return res
+23        
+24            res+=strs[0][i]
+25        
+26        return res
