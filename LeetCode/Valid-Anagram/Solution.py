b94e2a29276fa553ca06f7e7fe17cfb5828f022c
Rakhi Kumari <151828091+rakhi120805@users.noreply.github.com>
2026-09-20 22:42:46 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.py
+++ LeetCode/Valid-Anagram/Solution.py
@@ -16,10 +16,23 @@
 16        :type t: str
 17        :rtype: bool
 18        """
-19        s1="".join(sorted(s))
-20        t1="".join(sorted(t))
-21        if s1==t1:
-22            return True
+19        # s1="".join(sorted(s))
+20        # t1="".join(sorted(t))
+21        # if s1==t1:
+22        #     return True
 23        
-24        return False
-25        
+24        # return False
+25
+26
+27        if len(s)!=len(t):
+28            return False
+29        mpp1,mpp2={},{}
+30        for i in range(len(s)):
+31            mpp1[s[i]]=mpp1.get(s[i],0)+1
+32            mpp2[t[i]]=mpp2.get(t[i],0)+1
+33        for c in mpp1:
+34            if mpp1[c]!=mpp2.get(c,0):
+35                return False
+36            
+37        return True
+38        
