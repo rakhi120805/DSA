95e0b5ada6c699c9b824b2147218299fa92a00c0
Rakhi Kumari <151828091+rakhi120805@users.noreply.github.com>
2026-09-20 22:29:09 +0530

    Add solution for Valid Anagram

--- LeetCode/Valid-Anagram/Solution.py
+++ LeetCode/Valid-Anagram/Solution.py
@@ -1,19 +1,25 @@
 1class Solution(object):
 2    def isAnagram(self, s, t):
-3        if len(s)!=len(t):
-4            return False
-5        countS,countT={},{}
-6        for i in range (len(s)):
-7            countS[s[i]]=1+ countS.get(s[i],0)
-8            countT[t[i]]=1+ countT.get(t[i],0)
-9        for c in countS:
-10            if countS[c]!=countT.get(c,0):
-11                return False
+3        # if len(s)!=len(t):
+4        #     return False
+5        # countS,countT={},{}
+6        # for i in range (len(s)):
+7        #     countS[s[i]]=1+ countS.get(s[i],0)
+8        #     countT[t[i]]=1+ countT.get(t[i],0)
+9        # for c in countS:
+10        #     if countS[c]!=countT.get(c,0):
+11        #         return False
 12
-13        return True
+13        # return True
 14        """
 15        :type s: str
 16        :type t: str
 17        :rtype: bool
 18        """
-19        
+19        s1="".join(sorted(s))
+20        t1="".join(sorted(t))
+21        if s1==t1:
+22            return True
+23        
+24        return False
+25        
