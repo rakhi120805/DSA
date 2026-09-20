f5e987e183d4196758a7dad79fb0198d0277f548
Rakhi Kumari <151828091+rakhi120805@users.noreply.github.com>
2026-09-20 22:21:02 +0530

    Add solution for Contains Duplicate

--- LeetCode/Contains-Duplicate/Solution.py
+++ LeetCode/Contains-Duplicate/Solution.py
@@ -1,14 +1,31 @@
 1class Solution(object):
 2    def containsDuplicate(self, nums):
-3        hashset=set()
-4        for i in nums:
-5            if i in hashset:
-6                return True
+3        # hashset=set()
+4        # for i in nums:
+5        #     if i in hashset:
+6        #         return True
 7
-8            hashset.add(i)
-9        return False
+8        #     hashset.add(i)
+9        # return False
 10        """
 11        :type nums: List[int]
 12        :rtype: bool
 13        """
-14        
+14        hashset={}
+15        for i in range(len(nums)):
+16            hashset[nums[i]]=hashset.get(nums[i],0)+1
+17        
+18        for key in hashset:
+19            if hashset[key]>1:
+20                return True
+21        
+22        return False
+23
+24
+25
+26
+27
+28
+29
+30
+31        
